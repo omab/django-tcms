e90bf3bb801a77a4bfa8c0fd11996c1006ea615a
Matías Aguirre <matiasaguirre@gmail.com>
2011-09-10 20:33:49 -0300

    Initial setup.py file

--- README.rst
+++ README.rst
@@ -1,3 +1,5 @@
+Django tCMS an easy to setup CMS that integrates easily with django-admin.
+
 django-tcms was brought to life by Matías Aguirre while hacking a CMS framework
 for Mydeco_ while working on Insophia_.
 
---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Setup file for easy installation"""
+from os.path import join, dirname
+from setuptools import setup
+
+
+version = __import__('tcms').__version__
+
+LONG_DESCRIPTION = """
+Django tCMS an easy to setup CMS that integrates easily with django-admin.
+
+django-tcms was brought to life by Matías Aguirre while hacking a CMS framework
+for Mydeco while working on Insophia.
+"""
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
+setup(name='django-tcms',
+      version=version,
+      author=u'Matías Aguirre',
+      author_email='matiasaguirre@gmail.com',
+      description='Django tCMS.',
+      license='BSD',
+      keywords='django, cms, django-admin',
+      url='https://github.com/omab/django-tcms',
+      packages=['tcms',
+                'tcms.templatetags'],
+      package_data={
+          'tcms': [
+              'templates/cms/*.html',
+              'media/css/*.css',
+              'media/js/*.js',
+              'media/img/*.png',
+              'media/img/*.gif',
+              'media/img/*.gif',
+              'media/img/fancybox/*.gif',
+              'media/img/fancybox/*.png',
+          ]
+      },
+      long_description=long_description(),
+      install_requires=['django>=1.2.5'],
+      classifiers=['Framework :: Django',
+                   'Development Status :: 4 - Beta',
+                   'Topic :: Internet',
+                   'License :: OSI Approved :: BSD License',
+                   'Intended Audience :: Developers',
+                   'Environment :: Web Environment',
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7'])
